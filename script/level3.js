b492350290982ea9aba5471448ea67a44cac15ac
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-15 21:25:49 +0200

    Remove highscore

--- script/level3.js
+++ script/level3.js
@@ -13,8 +13,8 @@ const counterUp = () => {
     if (typeof (Storage) !== "undefined") {
         if (localStorage.counter) {
             localStorage.counter = Number(localStorage.counter) + 1;
-            document.getElementById("highscore").innerHTML = `Highscore - ${localStorage.counter}`;
-            document.getElementById("highscore").classList.add('highscore');
+            // document.getElementById("highscore").innerHTML = `Highscore - ${localStorage.counter}`;
+            // document.getElementById("highscore").classList.add('highscore');
 
         } else {
             localStorage.setItem("counter", 0);
