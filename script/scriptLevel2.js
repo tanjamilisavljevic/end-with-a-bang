3386c50d3b5b62e28fb9b64a3905130fcd500f48
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-07-15 12:09:49 +0200

    add changes css and trying a countdown js

--- script/scriptLevel2.js
+++ script/scriptLevel2.js
@@ -2,7 +2,36 @@
 // TODO: click to start game
 
 /*playGame = () => {*/
+    let hole = document.querySelector(".hole");
+
+    const resetStorage = () => {
+
+        localStorage.removeItem("counter");
+    };
+
+    /*countDownFunction = () => {
+        const timeLeft = 3;
+        const seconds = document.querySelector(".countdown");
+
+        const timerId = setInterval(countDownSeconds, 1000);
+
+        const countDownSeconds = () => {
+            if (timeLeft == -1) {
+                clearTimeout(timerId);
+                doSomething();
+            } else {
+                seconds.innerHTML = timeLeft;
+                timeLeft--;
+            }
+        };
+    };
+    countDownFunction();*/
+
+    // click button and button disappears and game starts
     document.querySelector(".btn-entergame").addEventListener("click", () =>{
+
+
+            resetStorage();
 
             let enterScreen = document.querySelector(".entergame");
 
@@ -17,37 +46,36 @@
             setTimeout(() =>{ enterScreen.remove()}, 1000);
 
             //  5 SECOND COUNTDOWN
-            
-            /*var timeLeft = 3;
-            var seconds = document.getElementById("startGame");
-
-            var timerId = setInterval(countdown, 1000);
-
-            const countdown = () => {
-                if (timeLeft == -1) {
-                    clearTimeout(timerId);
-                    doSomething();
+            const timeleft = 5;
+            const timer = setInterval(function(){
+                if(timeleft <= 0){
+                    clearInterval(timer);
+                    document.querySelector(".countdown").innerHTML = "Finished";
                 } else {
-                    seconds.innerHTML = timeLeft;
-                    timeLeft--;
+                    document.querySelector(".countdown").innerHTML = timeleft + " seconds remaining";
+                    timeleft--;
                 }
-            };*/
+            }, 500);
+    
+            console.log(timer);
             
 
             // start game when pressing start
 
         let pipe = document.querySelector(".pipe");
-        let hole = document.querySelector(".hole");
         let bert = document.querySelector(".bert");
         let jumping = 0;
 
         
-        // Make Bert jump
+        // Makes Bert jump 
         document.onclick = jump = () => {
+
             jumping = 1;
             let jumpCount = 0;
             let jumpInterval = setInterval(() =>{
+
                 let bertTop = parseInt(window.getComputedStyle(bert).getPropertyValue("top"));
+
                 if (( bertTop > 6 ) && ( jumpCount < 15 )) {
                     bert.style.top = ( bertTop - 5 ) + "px" ;
                 }
@@ -62,25 +90,27 @@
         
         //HIGHSCORE STORAGE
         const counterUp = () => {
+
             if ( typeof( Storage ) !== "undefined") {
+
                 if ( localStorage.counter ) {
                     localStorage.counter = Number( localStorage.counter ) + 1;
-                    document.getElementById("highscore").innerHTML = `Highscore ${localStorage.counter}`;
+                    document.getElementById("highscore").innerHTML = "HIGHSCORE : " + localStorage.counter;
                 } else {
-                    localStorage.setItem("counter", 0);
+                    localStorage.setItem("counter", 1);
                 }
             };
         };
 
         // PIPES
         hole.addEventListener('animationiteration', () => {
-            // random position of the hole
+            // random position of the pipes and hole
             let score = document.querySelector(".score"); 
             let random = -((Math.random() * 317) + 383);
             hole.style.top = random + "px";
             counterUp();
             score.innerHTML = "SCORE : " + localStorage.counter;
-            console.log(`${localStorage.counter}`);
+            console.log(localStorage.counter);
             });
 
 
@@ -90,34 +120,47 @@
         setInterval(() => {
             // makes Bert come down (gravity)
             let bertTop = parseInt(window.getComputedStyle(bert).getPropertyValue("top"));
+
             if (jumping == 0) {
                 bert.style.top = ( bertTop + 3 ) + "px";
             }
+
             let pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
             let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
             var cTop = -(800 - bertTop);
 
+            //Define spots you hit
             if(( bertTop > 640 )||(( pipeLeft < 20 ) && ( pipeLeft > -50 ) && (( cTop < holeTop )||( cTop > holeTop + 120 )))) {
-                //alert("You suck mate. Only " + localStorage.counter + " points");
+                
+                // what happens when you hit
+                // confirm text
                 const startOver = confirm ("You suck mate. Only " + localStorage.counter + " points. Do you want to try again?");
+
+                    // OK reload page
                     if(startOver == true){
                         location.reload();
                     }
+
+                    // cancel go back to home page
                     else {
                         document.location.href = "index.html";
                     }
-                const resetStorage = () => {
-                    localStorage.removeItem("counter");
-                }
-                resetStorage();
+                
                 bert.style.top = 100 + "px";
             }
 
-            if(localStorage.counter == 4){
+            // Define when  you win and go on to next level
+            if(localStorage.counter == 10){
+
+                // confirm text
                 const levelWin = confirm( "CONGRATULATIONS dude or dudette ! Press OK to go to the next level ! The next level is waiting for you. Cancel means back to the main page")
-                if(localStorage.counter == 4){
+                
+                // OK go to next level
+                if(localStorage.counter == 10){
                     document.location.href = "level3.html";
                 }
+
+                // cancel go to home page
                 else {
                     document.location.href = "index.html";
                 }
